19108d624fd2b06386df5846f691bff6cac5b109
Ashley Sands <ashsands1@amstrike.com>
2019-02-25 19:58:56 +0000

    inital commit for ball

---
+++ ball.h
@@ -0,0 +1,19 @@
+#pragma once
+class ball
+{
+public:
+	ball();
+	~ball();
+
+	int x = 1, y = 1;
+	int xPosition = 0;
+	int yPosition = 0;
+
+	int speed = 5;
+
+	bool movePositive = true;
+
+	
+
+};
+
