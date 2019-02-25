a5b93f8555bde4c8c83ec9950fdd86ad12cd8e9d
Ashley Sands <ashsands1@amstrike.com>
2019-02-25 22:41:24 +0000

    set move direction in contructor

--- ball.cpp
+++ ball.cpp
@@ -3,10 +3,14 @@
 
 
 ball::ball() : Transform::Transform()
-{}
+{
+	moveDirection = new Vector2(1, 0);
+}
 
 ball::ball(Vector2 pos, Vector2 size) : Transform::Transform(pos, size)
-{}
+{
+	moveDirection = new Vector2(1, 0);
+}
 
 ball::~ball()
 {
