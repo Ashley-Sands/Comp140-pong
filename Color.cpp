4e683983683eb3f50b61d8f520889a03d6ec07d0
Ashley Sands <ashsands1@amstrike.com>
2019-02-25 21:07:18 +0000

    added alpha to color

--- Color.cpp
+++ Color.cpp
@@ -5,8 +5,8 @@
 Color::Color()
 {
 
-	// set r,g,b to default 0
-	red = green = blue = 0;
+	// set r,g,b,a to default 0
+	red = green = blue = alpha = 0;
 
 }
 
@@ -15,7 +15,14 @@ Color::Color(int r, int g, int b)
 	red = r;
 	green = g;
 	blue = b;
+	alpha = 0;
 }
+
+Color::Color(int r, int g, int b, int a) : Color(r, g, b)
+{
+	alpha = a;
+}
+
 
 Color::~Color()
 {
--- Color.h
+++ Color.h
@@ -4,10 +4,12 @@ class Color
 public:
 	Color();
 	Color(int r, int g, int b);
+	Color(int r, int g, int b, int a);
 	~Color();
 
 	int red;
 	int green;
 	int blue;
+	int alpha;
 };
 
