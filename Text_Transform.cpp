74e38adcb043670710c159c0181fbfae78745427
Ashley Sands <ashsands1@amstrike.com>
2019-02-27 00:48:36 +0000

    Added Text Transform

---
+++ Text_Transform.cpp
@@ -0,0 +1,37 @@
+#include "Text_Transform.h"
+#include "libraries/SDL2_ttf-2.0.15/SDL_ttf.h"
+
+
+Text_Transform::Text_Transform() : Transform::Transform()
+{}
+
+Text_Transform::Text_Transform(std::string name) : Transform::Transform(name)
+{}
+
+Text_Transform::Text_Transform(Vector2 pos, Vector2 scale, std::string name) : Transform::Transform(pos, scale, name)
+{}
+
+
+Text_Transform::~Text_Transform()
+{
+}
+
+void Text_Transform::SetText(std::string text)
+{
+	textToDisplay = text;
+}
+
+void Text_Transform::Render(SDL_Renderer* renderer)
+{
+
+	TTF_Font* font = TTF_OpenFont("Sans.ttf", 24);
+	SDL_Color col = { color->red, color->green, color->blue };
+
+	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, textToDisplay.c_str(), col);
+	SDL_Texture* message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+	SDL_RenderCopy(renderer, message, NULL, &GetRect());
+
+	SDL_FreeSurface(surfaceMessage);
+	SDL_DestroyTexture(message);
+}
---
+++ Text_Transform.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Transform.h"
+class Text_Transform : public Transform
+{
+public:
+	Text_Transform();
+	Text_Transform(std::string name);
+	Text_Transform(Vector2 pos, Vector2 scale, std::string name);
+
+	Text_Transform();
+	~Text_Transform();
+
+	void SetText(std::string text);
+
+	virtual void Render(SDL_Renderer* renderer) override;
+
+private:
+	std::string textToDisplay;
+};
+
